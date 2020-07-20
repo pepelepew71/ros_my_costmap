ebbb83fe9dbd88181d453abea56a59e5a35fd3d0
yc.huang71 <yc.huang71@gmail.com>
2020-07-20 19:16:29 +0800

    use plt.imshow

--- scripts/local_viewer.py
+++ scripts/local_viewer.py
@@ -12,31 +12,25 @@ def cb(msg):
     width = msg.info.width
     resolution = msg.info.resolution
     data = np.array(object=msg.data, dtype=np.float)
-    DATA = np.reshape(a=data, newshape=(height, width))
+    data = np.reshape(a=data, newshape=(height, width))
+    DATA = data[::-1]
 
 def plot():
     fig = plt.gcf()
     ax = plt.gca()
     ax.cla()
-    ax.contourf(X, Y, DATA)
-    ax.scatter(0, 0, s=100, marker="s", color="white")
-    ax.axis("equal")
+    ax.imshow(X=DATA, cmap="jet")
     ax.grid()
     fig.canvas.draw()
     plt.tight_layout()
 
 if __name__ == "__main__":
 
-    _resol = 0.05
-    _x = np.arange(-5.0, 5.0, _resol)
-    _y = np.arange(-5.0, 5.0, _resol)
-    X, Y = np.meshgrid(_x, _y)
-    DATA = np.zeros(shape=(200, 200), dtype=float)
     plt.subplots()
     plt.ion()
     plt.show()
 
-    rospy.init_node(name="view_costmap", anonymous=False)
+    rospy.init_node(name="local_viewer", anonymous=False)
     rospy.Subscriber(name="/move_base/local_costmap/costmap", data_class=OccupancyGrid, callback=cb)
 
     rate = rospy.Rate(hz=2)
